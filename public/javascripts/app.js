e300a35a9d8c1bfb5d8ea93229e1ecb51b13ad61
coffeefan <cbwerbung@inaffect.net>
2015-03-17 20:09:22 +0100

    dragable include static

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,3 +3,32 @@ var ENTER_KEY = 13;
 $(function() {
     app.ScrumView=new app.ScrumView();
 });
+
+
+// Get all items from a container
+function getItems(container)
+{
+    var columns = [];
+
+    $(container+ ' ul.column').each(function(){
+        columns.push($(this).sortable('toArray').join(','));
+    });
+
+    return columns.join('|');
+}
+
+$(function() {
+
+    //var itemStr = getItems('#wrapper');
+    // itemStr = 'A,B|C,D|E'
+
+    $('.sortable-list').sortable({
+        connectWith: '.sortable-list'
+    });
+
+    /*$('#btn-get').click(function(){
+        alert(getItems('#example-1-2'));
+    });*/
+
+
+});
