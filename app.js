478ec7677f2fab922ced68cbf428eaaef24bbb90
coffeefan <cbwerbung@inaffect.net>
2015-04-20 10:31:39 +0200

    create im frontend

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var scrumpage = require('./routes/scrumpage');
 var scrumtasks = require('./routes/scrumtasks');
 
 var app = express();
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', scrumpage);
 app.use('/scrumtasks', scrumtasks);
 
 
